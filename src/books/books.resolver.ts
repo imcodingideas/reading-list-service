224112d4ab66a138fe09ae78c6e66cc00a6da009
Joseph Chambers <joseph@michael-chambers.com>
2024-10-25 15:26:24 -0700

    add draft schema and list books resolver

--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -1,14 +1,30 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Book, Status } from 'src/graphql';
 
-const booksInMemory = [
+const booksInMemory: Book[] = [
   {
-    name: 'Book 1',
+    id: '1',
+    title: 'Book 1',
+    author: 'Author 1',
+    status: Status.COMPLETED,
+    rating: 4,
+    notes: 'Notes for Book 1',
   },
   {
-    name: 'Book 2',
+    id: '2',
+    title: 'Book 2',
+    author: 'Author 2',
+    status: Status.IN_PROGRESS,
+    rating: 3,
+    notes: 'Notes for Book 2',
   },
   {
-    name: 'Book 3',
+    id: '3',
+    title: 'Book 3',
+    author: 'Author 3',
+    status: Status.NOT_STARTED,
+    rating: 2,
+    notes: 'Notes for Book 3',
   },
 ];
 
@@ -17,7 +33,7 @@ export class BooksResolver {
   constructor() {}
 
   @Query()
-  async getBooks() {
+  async listBooks() {
     return booksInMemory;
   }
 }
--- src/graphql.ts
+++ src/graphql.ts
@@ -8,12 +8,58 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export enum Status {
+    NOT_STARTED = "NOT_STARTED",
+    IN_PROGRESS = "IN_PROGRESS",
+    COMPLETED = "COMPLETED"
+}
+
+export class CreateBookInput {
+    title: string;
+    author: string;
+    status?: Nullable<Status>;
+    rating?: Nullable<number>;
+    notes?: Nullable<string>;
+}
+
+export class UpdateBookInput {
+    id: string;
+    title?: Nullable<string>;
+    author?: Nullable<string>;
+    status?: Nullable<Status>;
+    rating?: Nullable<number>;
+    notes?: Nullable<string>;
+}
+
+export class DeleteBookInput {
+    id: string;
+}
+
+export class GetBookInput {
+    id: string;
+}
+
 export class Book {
-    name: string;
+    id: string;
+    title: string;
+    author: string;
+    status: Status;
+    rating?: Nullable<number>;
+    notes?: Nullable<string>;
 }
 
 export abstract class IQuery {
-    abstract getBooks(): Book[] | Promise<Book[]>;
+    abstract getBook(input: GetBookInput): Nullable<Book> | Promise<Nullable<Book>>;
+
+    abstract listBooks(): Nullable<Book[]> | Promise<Nullable<Book[]>>;
+}
+
+export abstract class IMutation {
+    abstract createBook(input: CreateBookInput): Nullable<Book> | Promise<Nullable<Book>>;
+
+    abstract updateBook(input: UpdateBookInput): Nullable<Book> | Promise<Nullable<Book>>;
+
+    abstract deleteBook(input: DeleteBookInput): Nullable<boolean> | Promise<Nullable<boolean>>;
 }
 
 type Nullable<T> = T | null;
